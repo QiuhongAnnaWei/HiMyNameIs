24a218f9c6479d2b46849c87de7e4ea0dcb6ea42
Qiuhong Anna Wei <54920263+QiuhongAnnaWei@users.noreply.github.com>
2021-01-25 01:24:20 +0800

    adding similar word function

--- server/generate.py
+++ server/generate.py
@@ -1,11 +1,15 @@
 from textgenrnn import textgenrnn
 
-def generateOne(pre, temp):
+model_name = 'allnames2015-'
+dirpath = './models/allnames2015-/' # from server/
+textgen = textgenrnn(weights_path=f'{dirpath}{model_name}_weights.hdf5',
+                    vocab_path=f'{dirpath}/{model_name}_vocab.json',
+                    config_path=f'{dirpath}/{model_name}_config.json')
+
+def generateOne(pre, temp, similar):
     p = None if pre=="" else pre
     t = 0.5 if temp==0 else temp
-    model_name = 'allnames2015-'
-    dirpath = './models/allnames2015-/' # from server/
-    textgen = textgenrnn(weights_path=f'{dirpath}{model_name}_weights.hdf5',
-                        vocab_path=f'{dirpath}/{model_name}_vocab.json',
-                        config_path=f'{dirpath}/{model_name}_config.json')
-    return textgen.generate(temperature=t, prefix=p, n=5, max_gen_length=100, return_as_list=True)[0]
+    if similar == "":
+        return textgen.generate(temperature=t, prefix=p, n=5, max_gen_length=100, return_as_list=True)[0]
+    else:
+        pass
